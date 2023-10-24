a197257f9a95518f2d63ec193291ee1a3553048d
student-studies-everyday <student.studies.everyday@gmail.com>
2023-10-24 22:44:47 +0900

    re

--- jq/14/step6/js.js
+++ jq/14/step6/js.js
@@ -1,22 +1,23 @@
 $(() => {
 	const sections = $('section');
-	let speed = Math.floor($(window).height() * 0.9);
+	let speed = Math.floor($(window).height() * 0.2);
 	let topArr = [];
 	let winSCT;
 	sections.each((idx, section) => {
 		$(section).addClass(`bg${idx + 1}`);
-		const sectionTop = $(section).offset().top;
+		const sectionTop = Math.floor($(section).offset().top);
 		topArr.push(sectionTop);
 	});
-	console.log(topArr);
+	console.log(topArr, speed);
 	/* 스크롤함수 */
 	$(window).on('scroll', () => {
-		winSCT = $(window).scrollTop();
-		if (winSCT > topArr[0] && winSCT < topArr[1] - speed) {
+		winSCT = Math.floor($(window).scrollTop());
+
+		if (winSCT > topArr[0]) {
 			sections.eq(0).find('.box').css('transform', 'translateX(0%)');
 		}
-
-		if (winSCT > topArr[1] && winSCT < topArr[2] - speed) {
+		if (winSCT > topArr[1] - speed) {
+			console.log(winSCT > topArr[1] - speed, winSCT, topArr[1] - speed);
 			sections.eq(1).find('.bg3').stop().delay(0).animate({ top: '5vw', opacity: 1 }, 500, 'swing');
 			sections.eq(1).find('.bg4').stop().delay(200).animate({ top: '0vw', opacity: 1 }, 800, 'swing');
 			sections.eq(1).find('.bg5').stop().delay(300).animate({ top: '-5vw', opacity: 1 }, 1100, 'swing');
@@ -26,8 +27,7 @@ $(() => {
 			console.log(winSCT > topArr[2] && winSCT < topArr[3]);
 			sections.eq(2).addClass('is-animated');
 		}
-		if (winSCT > topArr[3] - speed && winSCT < topArr[4]) {
-			console.log(topArr[4], winSCT);
+		if (winSCT > topArr[3] - speed) {
 			sections.eq(3).addClass('is-animated').siblings().removeClass('is-animated');
 			pipScroll();
 		}
@@ -38,9 +38,11 @@ $(() => {
 	});
 
 	function pipScroll() {
-		const devices = ['.mockup.pc', '.mockup.mobile'];
-		$.each(devices, function (i, deviceEl) {
-			let device = $(deviceEl);
+		//const devices = ['.mockup.pc', '.mockup.mobile'];
+		const devices = $('.mockup.pc, .mockup.mobile');
+		//$.each(devices, function (i, deviceEl) {
+		devices.each(function (i, deviceEl) {
+			let device = $(this);
 			let screen = device.find('.mask>img');
 			let mask = device.find('.mask');
 			let heightDifference = screen.innerHeight() - mask.innerHeight();
