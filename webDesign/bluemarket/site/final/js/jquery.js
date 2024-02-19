2819555213618793cf8c4ebe5f5934d16d81d96d
qwerewqwerew <qwerew0@gmail.com>
2024-02-19 09:05:40 +0900

    fix

--- webDesign/bluemarket/site/final/js/jquery.js
+++ webDesign/bluemarket/site/final/js/jquery.js
@@ -1,19 +1,19 @@
 // menu
 const depth1 = $('.depth1');
-depth1.hover(
-	() => {
+depth1.on({
+	mouseover: function () {
 		depth1.find('.depth2').each((i, o) => {
 			$(o).css('display', 'block');
 			$('header').addClass('on');
 		});
 	},
-	() => {
+	mouseleave: function () {
 		depth1.find('.depth2').each((i, o) => {
 			$(o).css('display', 'none');
 			$('header').removeClass('on');
 		});
-	}
-);
+	},
+});
 
 // slide
 const slides = $('.slide_wrap li');
@@ -37,7 +37,7 @@ const panels = $('.panels>div');
 tabBtn.on('click', function (e) {
 	e.preventDefault();
 	let tg = $(this).index();
-	panels.hide()
+	panels.hide();
 	tabBtn.removeClass('on');
 	tabBtn.eq(tg).addClass('on');
 	panels.eq(tg).show();
@@ -45,17 +45,13 @@ tabBtn.on('click', function (e) {
 
 //popup
 
-$('#panel1 .row.open').on('click',function(e){
+$('#panel1 .row.open').on('click', function (e) {
 	e.preventDefault();
-
-})
-
-
-addEventListener('click',function(){
-	document.querySelector('.popup').style.display='block';
-	document.querySelector('.overlay').classList.add('open');
-})
-document.querySelector('.close').addEventListener('click',function(){
-	document.querySelector('.popup').style.display='none';
-	document.querySelector('.overlay').classList.remove('open');
-})
+	$('.popup').show();
+	$('.overlay').show();
+});
+$('.close').on('click', function (e) {
+	e.preventDefault();
+	$('.popup').hide();
+	$('.overlay').hide();
+});
